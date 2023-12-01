e0591d78d1f513ea5c17e0baae2a69811c06d4e5
Pierr <pierredolivet@live.fr>
2023-12-01 19:41:33 +0100

    add year 2022

---
+++ 2022/01-12/main.py
@@ -0,0 +1,21 @@
+def star_one():
+    
+    
+    return result
+
+# Premier EXO
+resultat_final = star_one()
+print("exo 1 = " + str(resultat_final))
+
+#####################################################################################################
+
+import re
+
+def star_two():
+    
+    
+    return total
+
+# Deuxième EXO
+resultat_final2 = star_two()
+print("exo 2 = " + str(resultat_final2))
