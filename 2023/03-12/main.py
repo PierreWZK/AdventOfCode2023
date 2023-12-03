24f031349246c1d1537293398ab619760c1992ec
Pierr <pierredolivet@live.fr>
2023-12-03 17:26:24 +0100

    Day 3 solved

--- 2023/03-12/main.py
+++ 2023/03-12/main.py
@@ -2,14 +2,39 @@ import re
 
 def star_one():
     # Ouvrir un fichier et en récupérer les lignes
-    lines = (open("input.txt")).readlines()
+    lines = open("input.txt").read().splitlines()
 
     # Créer les variables qui vont contenir les données
     result = 0
     
-    for line in lines: 
-        1
-    # Additionnez les pieces de moteurs qui ont un numéro de série
+    # Obtenir la hauteur et la largeur de la grille    
+    height = len(lines)
+    width = len(lines[0])
+    
+    # Pour chaque ligne et caractère dans la grille
+    for y1 in range(height):
+        # Utiliser un itérateur pour parcourir les colonnes
+        xIter = iter(range(width))
+        for x1 in xIter:
+            # Si le caractère est un chiffre
+            if lines[y1][x1].isdigit():
+                # x1 représente le début du nombre, endX1 représente la fin du nombre
+                endX1 = next(xIter, width)
+                # Trouver la fin du nombre
+                while endX1 < width and lines[y1][endX1].isdigit():
+                    endX1 = next(xIter, width)
+                
+                # Fonction pour trouver les nombres adjacents à des symboles
+                def trouver_caractere():
+                    for y in range(y1 - 1, min(y1 + 2, height)):
+                        for x in range(x1 - 1, min(endX1 + 1, width)):
+                            # Vérifier si le caractère est un symbole et non un chiffre
+                            if lines[y][x] != '.' and not lines[y][x].isdigit():
+                                return True
+                                
+                # Si des nombres adjacents sont trouvés, ajouter le nombre au résultat total
+                if trouver_caractere():
+                    result += int(lines[y1][x1:endX1])
     
     return result
 
@@ -23,12 +48,48 @@ print("exo 1 = " + str(resultat_final))
 
 def star_two():
     # Ouvrir un fichier et en récupérer les lignes
-    lines = (open("input.txt")).readlines()
+    lines = open("input.txt").read().splitlines()
 
-    # Créer les variables qui vont contenir les données
+    # Variable created
     result = 0
     
-    
+    # Obtenir la hauteur et la largeur de la grille
+    hauteur = len(lines)
+    largeur = len(lines[0])
+
+    # Initialiser un dictionnaire pour stocker les engrenages et leurs valeurs
+    engrenages = {}
+
+    # Pour chaque ligne et caractère dans la grille
+    for y1 in range(hauteur):
+        # Utiliser un itérateur pour parcourir les colonnes
+        xIter = iter(range(largeur))
+        for x1 in xIter:
+            # Si le caractère est un chiffre
+            if lines[y1][x1].isdigit():
+                # x1 représente le début du nombre, x2 représente la fin du nombre
+                x2 = next(xIter, largeur)
+                # Trouver la fin du nombre
+                while x2 < largeur and lines[y1][x2].isdigit():
+                    x2 = next(xIter, largeur)
+
+                # Trouver les engrenages adjacents
+                for y in range(y1 - 1, min(y1 + 2, hauteur)):
+                    for x in range(x1 - 1, min(x2 + 1, largeur)):
+                        if lines[y][x] == '*':
+                            # Ajouter la valeur du nombre à l'engrenage correspondant dans le dictionnaire
+                            if (y, x) not in engrenages:
+                                engrenages[(y, x)] = []
+                            engrenages[(y, x)].append(int(lines[y1][x1:x2]))
+
+    # Compter les engrenages incorrects
+    for valeurs in engrenages.values():
+        if len(valeurs) > 1:
+            ratio = 1
+            for valeur in valeurs:
+                ratio *= valeur
+            result += ratio
+
     return result
 
 # Deuxième EXO
@@ -36,29 +97,3 @@ resultat_final2 = star_two()
 
 # RESULT ATTENDU : 78915902
 print("exo 2 = " + str(resultat_final2))
-
-grid = open("input.txt").read().splitlines()
-cs = set()
-
-for r, row in enumerate(grid):
-    for c, ch in enumerate(row):
-        if ch.isdigit() or ch == ".":
-            continue
-        for dr in range(r - 1, r + 2):
-            for dc in range(c - 1, c + 2):
-                if dr < 0 or dr >= len(grid) or dc < 0 or dc >= len(grid[dr]) or not grid[dr][dc].isdigit():
-                    continue
-                while dc > 0 and grid[dr][dc - 1].isdigit():
-                    dc -= 1
-                cs.add((dr, dc))
-
-ns = []
-
-for r, c in cs:
-    s = ""
-    while c < len(grid[r]) and grid[r][c].isdigit():
-        s += grid[r][c]
-        c += 1
-    ns.append(int(s))
-
-print(sum(ns))
