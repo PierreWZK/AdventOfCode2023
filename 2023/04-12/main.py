554da106d876de67d5e051248df4173f90075d99
Pierre Dolivet <pierre.dolivet@cnsi-sd.fr>
2023-12-04 11:27:41 +0100

    Prepa Day4

--- 2023/04-12/main.py
+++ 2023/04-12/main.py
@@ -1,5 +1,17 @@
+# Part 1: 15268
+# Part 2: 6283755
+
+import re
+
 def star_one():
+    # Ouvrir un fichier et en récupérer les lignes
+    lines = (open("input.txt")).readlines()
+
+    # Créer les variables qui vont contenir les données
+    result = 0
     
+    
+    # Additionnez les pieces de moteurs manquantes
     
     return result
 
@@ -9,12 +21,15 @@ print("exo 1 = " + str(resultat_final))
 
 #####################################################################################################
 
-import re
-
 def star_two():
+    # Ouvrir un fichier et en récupérer les lignes
+    lines = (open("input.txt")).readlines()
+
+    # Créer les variables qui vont contenir les données
+    result = 0
     
     
-    return total
+    return result
 
 # Deuxième EXO
 resultat_final2 = star_two()
