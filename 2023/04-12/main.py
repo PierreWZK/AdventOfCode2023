49aad08b91a06a1569f542ded05e3545305029e4
Pierre Dolivet <pierre.dolivet@cnsi-sd.fr>
2023-12-04 11:53:39 +0100

    correction and updated template

--- 2023/04-12/main.py
+++ 2023/04-12/main.py
@@ -10,8 +10,36 @@ def star_one():
     # Créer les variables qui vont contenir les données
     result = 0
     
-    
-    # Additionnez les pieces de moteurs manquantes
+    for line in lines :
+        # Déclaration des variables
+        nombre_gagnant = []
+        liste = []
+        n_gagnant = -1
+        
+        # Mise à jour des variables
+        line = line.replace('\n', '')
+        array_lines = line.split(': ')
+
+        # Récupération des nombres gagnants et de la liste
+        AllNumbers = array_lines[1]
+        nombre_gagnant = AllNumbers.split(' | ')[0]
+        nombre_gagnant = nombre_gagnant.split(' ')
+        liste = AllNumbers.split(' | ')[1]
+        liste = liste.split(' ')
+
+        # Suppression des espaces vides
+        nombre_gagnant = list(filter(None, nombre_gagnant))
+        liste = list(filter(None, liste))
+
+        # Comparaison des nombres gagnants et de la liste
+        # Si un nombre gagnant est dans la liste, alors on ajoute 2^n à result (1, 1, 2, 4, 8, 16, 32, 64, 128, 256)
+        for i in range(len(liste)):
+            if liste[i] in nombre_gagnant:
+                nn_gagnant = n_gagnant
+                if n_gagnant == -1:
+                    nn_gagnant = 0
+                result += 2 ** nn_gagnant
+                n_gagnant += 1
     
     return result
 
